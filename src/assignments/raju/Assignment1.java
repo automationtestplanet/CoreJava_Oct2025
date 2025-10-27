af5b9e2113cb5aabe71e0bd2ac6c451f03c38d3f
Automation Test Planet <automationtesttrainings@gmail.com>
2025-10-27 08:01:46 +0530

    raju assignment1 changes

--- src/assignments/raju/Assignment1.java
+++ src/assignments/raju/Assignment1.java
@@ -4,10 +4,15 @@ public class Assignment1 {
 
 	// Write a reusable code to print the factorial value of a number
 	// num: 5 -> 5X4X3X2X1 -> 120
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void printFactorialValueOfGivenNumber(int num) {
+		int sum = 1;
+		for (int i = 1; i <= num; i++) {
+			sum = sum * i;
+		}
+		System.out.println(num + " Factorail Value: " + sum);
 	}
 
+	public static void main(String[] args) {
+		printFactorialValueOfGivenNumber(25);
+	}
 }
