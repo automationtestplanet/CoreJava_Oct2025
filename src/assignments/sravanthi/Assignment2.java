96cee6ba3e5d64090630f481392b49bac72730a1
Automation Test Planet <automationtesttrainings@gmail.com>
2025-10-28 08:38:00 +0530

    new assignments2

---
+++ src/assignments/sravanthi/Assignment2.java
@@ -0,0 +1,13 @@
+package assignments.sravanthi;
+
+public class Assignment2 {
+	
+	// Write a reusable program to sort an array in a reverse order
+	// ex: int[] arr = {3,5,2,1,4} -> o/p: {5,4,3,2,1}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/assignments/vijay/Assignment2.java
@@ -0,0 +1,15 @@
+package assignments.vijay;
+
+public class Assignment2 {
+
+	
+	// Write a reusable program to print missed numbers in an array
+	// ex: int[] arr = {1,2,5,7,9}  -> o/p: 3,4,6,8
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
