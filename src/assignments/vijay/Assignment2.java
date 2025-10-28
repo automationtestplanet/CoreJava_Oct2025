a3eb5cb266b3cd76eff29094581f13051b69aab3
vijaysayyaparaju <varmasayyaparaju42@gmail.com>
2025-10-28 23:44:04 +0530

    vijay Assignment2 was done

--- src/assignments/vijay/Assignment2.java
+++ src/assignments/vijay/Assignment2.java
@@ -5,11 +5,31 @@ public class Assignment2 {
 	
 	// Write a reusable program to print missed numbers in an array
 	// ex: int[] arr = {1,2,5,7,9}  -> o/p: 3,4,6,8
-	
+	public static void MissedNumbersInArray(int[] arr) {
+		
+		boolean first = true;
+		 
+		for (int i =0; i < arr.length-1; i++) {
+			for(int j =arr[i]+1; j <arr[i +1]; j++) {
+				if (!first){
+					System.out.print(",");
+				}
+				System.out.print(j);
+				first = false;
+			}
+				
+		}
+		
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		MissedNumbersInArray(new int[]{1,2,5,7,9});
+		System.out.println();
+		
+		MissedNumbersInArray(new int[]{22,25,28});
+		
+		
 	}
 
 }
