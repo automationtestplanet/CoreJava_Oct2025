59ea7629eb0fc1b68ccdbdf6d6c08a3689b5a8ed
Maheshwari <mahi23neeli@gmail.com>
2025-10-28 14:34:50 +0530

    Assignment1.java

--- src/assignments/maheshwari/Assignment1.java
+++ src/assignments/maheshwari/Assignment1.java
@@ -5,9 +5,21 @@ public class Assignment1 {
 	// Write a Java Program to print the Fibonacci series for given times
 	//Ex: num: 5  -> 0 1 1 2 3 5 8
 	// Ex: num: 4  -> 0 1 1 2 3 5
+	public void fibonacci(int num){
+		int num1=0;
+		int num2=1;
+		for(int i=1;i<=num;i++){
+			System.out.println(num1+" ");
+			int res=num1+num2;
+			num1=num2;
+			num2=res;
+			}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Assignment fb= new Assignment();
+		fb.fibonacci(5);
 
 	}
 
