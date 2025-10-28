262bc7e4242991b0207635f086694336da35e49b
vijaysayyaparaju <varmasayyaparaju42@gmail.com>
2025-10-28 06:55:03 +0530

    changes done according to comments on my review

--- src/assignments/vijay/Assignment1.java
+++ src/assignments/vijay/Assignment1.java
@@ -6,26 +6,7 @@ public class Assignment1 {
 	// Ex: 25 -> Buzz number
 	// Ex: 13 -> not Buzz Number
 
-	public static int number = 7;
-	
-	int num = 29;
-	
-	
-	public static void BuzzNumbers() {
-		if (number % 10 == 7 || number % 7 == 0) {
-			System.out.println(number + ": is a Buzz Number");
-		} else {
-			System.out.println(number + ": is not a Buzz Number");
-		}
-	}
-	
-	public void BuzzNumber() {
-		if (num % 10 == 7 || num % 7 == 0) {
-			System.out.println(num + ": is a Buzz Number");
-		} else {
-			System.out.println(num + ": is not a Buzz Number");
-		}
-	}
+
 	
 	public static void BuzzNumber(int num) {
 		if (num % 10 == 7 || num % 7 == 0) {
@@ -38,11 +19,6 @@ public class Assignment1 {
 	
 
 	public static void main(String[] args) {
-		
-		BuzzNumbers();
-		
-		Assignment1 number = new Assignment1();
-		number.BuzzNumber();
 		
 		BuzzNumber(63);
 		
