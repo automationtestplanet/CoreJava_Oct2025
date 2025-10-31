9d7f202c64fc31f038007676ea5e6389f4ec67b0
vijaysayyaparaju <varmasayyaparaju42@gmail.com>
2025-10-31 19:27:08 +0530

    Assignment2 changes done

--- src/assignments/vijay/Assignment3.java
+++ src/assignments/vijay/Assignment3.java
@@ -5,9 +5,27 @@ public class Assignment3 {
 	// Write a program to split the given sentence string to words and count each word length
 	// ex: This Hello Java Program -> o/p: 4 5 4 7
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void  WordLengthCounter2(String sentence) {
+		
+		int count = 0;
+		
+		System.out.print("Word lengths: ");
+		for(int i =0; i <sentence.length();i++) {
+			char ch = sentence.charAt(i);
+			
+			if (ch != ' '){
+				count++;		
+			}else {
+				System.out.print(count +" ");
+				count = 0;
+			}
+		}
+		System.out.print(count);
 	}
-
+	
+	public static void main(String[] args) {
+		String sentence = "This Hello Java Program";
+		WordLengthCounter2(sentence);
+	}
 }
+
