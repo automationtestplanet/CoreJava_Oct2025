b64340cb6bdb113e08f7fbd437bce8a12a4d7b7b
Automation Test Planet <automationtesttrainings@gmail.com>
2025-10-31 11:48:58 +0530

    new Assignments

---
+++ src/assignments/maheshwari/Assignment2.java
@@ -0,0 +1,13 @@
+package assignments.maheshwari;
+
+public class Assignment2 {
+
+	// Write a program to sort an array in ascending order
+	// ex: int[] arr = {3,5,2,1,4} -> o/p: {1,2,3,4,5}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/assignments/vijay/Assignment3.java
@@ -0,0 +1,13 @@
+package assignments.vijay;
+
+public class Assignment3 {
+
+	// Write a program to split the given sentence string to words and count each word length
+	// ex: This Hello Java Program -> o/p: 4 5 4 7
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
